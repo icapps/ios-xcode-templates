72b8b0764d06e7e5ee43de8faeb97c6fdff60668
Jelle Vandebeeck <jelle@fousa.be>
2018-02-14 21:33:25 +0100

    Add the MVVM file template.

---
+++ MVVM.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    // MARK: - Internal
+    
+    private let viewModel: ___VARIABLE_productName___ViewModel = ___VARIABLE_productName___ViewModel()
+
+}
